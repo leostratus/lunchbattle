759d8fa408503c2fc19d88eee8b8656d9ab1a015
Angelina Fabbro <angelina@steamclocksw.com>
2012-03-10 00:47:39 -0800

    Initial commit of LUNCH BATTLE! RAAAAAAAAAARRRRRR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchBattle::Application.config.secret_token = '488ad73e4646c98dbffe6db5f94ba2cb6e2e5cdf30e5136ee9777ccbc174412254dd78fb5bf6e0b321cc79e4d51cb4417979c345d281005847408a5b2a234217'
