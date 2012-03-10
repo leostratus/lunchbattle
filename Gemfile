69092070b8488694ae06190ca8b62fb90de81927
Godfrey Chan <kfc5@sfu.ca>
2012-03-10 14:49:23 -0800

    Yelp

--- Gemfile
+++ Gemfile
@@ -6,8 +6,8 @@ gem 'rails', '3.2.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem "geocoder"
-#gem "yelp"
-gem "yelpster"
+gem "yelp"
+# gem "yelpster"
 # gem "devise"
 
 
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,14 @@
+YELP_YWSID = 'o7f0BvjY65VTw_IQHuJkhA'
+
+YELP_V2_KEYS = {
+  :consumer_key => 'EPuiJ7Q6CZpLuL5m8y2anA',
+  :consumer_secret => 'Z6YQ5O66VR1bdFsStJ7oRvrv8N8',
+  :token => 'w8cASHycaPCBEmOee_KoAUsbE7mx8GZZ',
+  :token_secret => '5_zTuD8BeywHvqAsCf4ZMo_ZH7k'
+}
+
+YELP_RESTAURANT_CATEGORIES = [
+  
+  
+  
+]
