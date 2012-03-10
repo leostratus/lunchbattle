7728452c6d09b909a0ab6d0846aac3c4c3279265
Godfrey Chan <kfc5@sfu.ca>
2012-03-10 12:18:43 -0800

    Added searches

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Foodfight::Application.routes.draw do
+  resources :searches
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Foodfight::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'searches#new'
 
   # See how all your routes lay out with "rake routes"
 
